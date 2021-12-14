0737520e8b95f13e6c4afeafd728336da90905f0
hyun5292 <tytyjacob@naver.com>
2021-12-15 00:39:36 +0900

    Youtube 사이트 만들기
    환경설정 및 동영상 업로드까지 성공
    후 썸네일 업로드 format 오류 및 ffprobe 오류 상태

---
+++ youtube/client/src/components/App.js
@@ -0,0 +1,37 @@
+import React, { Suspense } from 'react';
+import { Route, Switch } from "react-router-dom";
+import Auth from "../hoc/auth";
+// pages for this product
+import LandingPage from "./views/LandingPage/LandingPage.js";
+import LoginPage from "./views/LoginPage/LoginPage.js";
+import RegisterPage from "./views/RegisterPage/RegisterPage.js";
+import NavBar from "./views/NavBar/NavBar";
+import Footer from "./views/Footer/Footer"
+import VideoUploadPage from "./views/VideoUploadPage/VideoUploadPage"
+
+//null   Anyone Can go inside
+//true   only logged in user can go inside
+//false  logged in user can't go inside
+
+function App() {
+  return (
+    <Suspense fallback={(<div>Loading...</div>)}>
+      <NavBar />
+      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+        <Switch>
+          {/*
+          null이면 아무나, true는 로그인 한 사람만
+          http://localhost:3000/video/upload 이렇게 들어갈 수 있음
+          */}
+          <Route exact path="/" component={Auth(LandingPage, null)} />
+          <Route exact path="/login" component={Auth(LoginPage, false)} />
+          <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route exact path="/video/upload" component={Auth(VideoUploadPage, true)} />
+        </Switch>
+      </div>
+      <Footer />
+    </Suspense>
+  );
+}
+
+export default App;
