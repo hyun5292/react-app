227fdf54082c03820a13562e14076f21936f1798
hyun5292 <tytyjacob@naver.com>
2022-01-11 22:59:30 +0900

    배열에 항목 수정하기

--- first/src/components/UserList.js
+++ first/src/components/UserList.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
-function User({ user, onRemove }) {
+function User({ user, onRemove, onToggle }) {
     return (
+        /*
         <div>
             <b>{user.username}</b> <span>({user.email})</span>
             <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
+        */
+       <div>
+            <b
+                style={{
+                    cursor: 'pointer',
+                    color: user.active ? 'green' : 'black'
+                }}
+                onClick={() => onToggle(user.id)}
+            >
+                {user.username}
+            </b>
+            <span>({user.email})</span>
+            <button onClick={() => onRemove(user.id)}>삭제</button>
+       </div>
     );
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
 /*
     return (
         <div>
@@ -37,7 +52,12 @@ function UserList({ users, onRemove }) {
     return (
         <div>
             {users.map(user => (
-                <User user={user} key={user.id} onRemove={onRemove} />
+                <User 
+                    user={user} 
+                    key={user.id} 
+                    onRemove={onRemove}
+                    onToggle={onToggle} 
+                />
             ))}
         </div>
     );
