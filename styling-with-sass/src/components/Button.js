ababe9cf7247cdb7e6053dfd14a28a588ff07d12
hyun5292 <tytyjacob@naver.com>
2022-01-14 00:19:54 +0900

    하나하나 넣어주기 귀찮으면 ...rest
    function Button({ children, size, color, outline, fullWidth, ...rest }) {
    
    =>
    
    <Button size="large" onClick={() => console.log('클릭됐다!')}>BUTTON</Button>

--- styling-with-sass/src/components/Button.js
+++ styling-with-sass/src/components/Button.js
@@ -2,10 +2,13 @@ import React from 'react';
 import classNames from 'classnames';
 import './Button.scss';
 
-function Button({ children, size, color, outline, fullWidth }) {
+function Button({ children, size, color, outline, fullWidth, ...rest }) {
   return (
     //outline을 객체 안에 넣으면 true일 때에만 적용
-    <button className={classNames('Button', size, color, { outline, fullWidth })}>
+    <button 
+      className={classNames('Button', size, color, { outline, fullWidth })}
+      {...rest}
+    >
       {children}
     </button>
   );
