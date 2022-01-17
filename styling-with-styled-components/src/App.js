1f563a0ba5ed456c32e28c1a1df05798c038bdc5
hyun5292 <tytyjacob@naver.com>
2022-01-17 20:21:38 +0900

    styled-components 리팩토링 전

--- styling-with-styled-components/src/App.js
+++ styling-with-styled-components/src/App.js
@@ -1,9 +1,50 @@
-import './App.css';
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import Button from './components/Button';
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
+const ButtonGroup = styled.div`
+  & + & {
+    margin-top: 1rem;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider
+      theme={{
+        palette: {
+          blue: '#228be6',
+          gray: '#495057',
+          pink: '#f06595'
+        }
+      }}
+    >
+      <AppBlock>
+        <ButtonGroup>
+          <Button size="large">BUTTON</Button>
+          <Button>BUTTON</Button>
+          <Button size="small">BUTTON</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button color="gray" size="large">BUTTON</Button>
+          <Button color="gray">BUTTON</Button>
+          <Button color="gray" size="small">BUTTON</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button color="pink" size="large">BUTTON</Button>
+          <Button color="pink">BUTTON</Button>
+          <Button color="pink" size="small">BUTTON</Button>
+        </ButtonGroup>
+      </AppBlock>
+    </ThemeProvider>
   );
 }
 
